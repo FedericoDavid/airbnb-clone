41c875dbf91c4a31b14e5c5f90abf03a5e8a95c9
FedericoCIB <141157787+FedericoCIB@users.noreply.github.com>
2023-09-04 11:20:40 -0300

    feat: adding sign up functionallity, w prisma and nextauth setup

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
