c99b3ce95f43f5528ba0fb67abf2f037b48f2b46
Federico_Enriquez <fedenri98@gmail.com>
2023-10-24 23:24:57 -0300

    feat: adding general code improves and also github and google Oauth

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 
 import userRegisterModal from "@/app/hooks/useRegisterModal";
+import { signIn } from "next-auth/react";
 import Input from "../inputs/Input";
 import Button from "../Button";
 
@@ -87,13 +88,13 @@ const RegisterModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn("google")}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
