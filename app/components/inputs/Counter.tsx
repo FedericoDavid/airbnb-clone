58f44301c925863c7fb5c63b322ac30088fb4515
Federico_Enriquez <fedenri98@gmail.com>
2023-10-27 16:13:27 -0300

    feat: adding some code improves o counter w new vary button

--- app/components/inputs/Counter.tsx
+++ app/components/inputs/Counter.tsx
@@ -26,6 +26,34 @@ const Counter: React.FC<CounterProps> = ({
     onChange(value - 1);
   }, [onChange, value]);
 
+  const VaryButton = ({
+    onClick,
+    isMinor,
+  }: {
+    onClick: () => void;
+    isMinor?: boolean;
+  }) => (
+    <div
+      onClick={onClick}
+      className="
+      w-10
+      h-10
+      rounded-full
+      border-[1px]
+      border-neutral-400
+      flex
+      items-center
+      justify-center
+      text-neutral-600
+      cursor-pointer
+      hover:opacity-80
+      transition
+    "
+    >
+      {isMinor ? <AiOutlineMinus /> : <AiOutlinePlus />}
+    </div>
+  );
+
   return (
     <div className="flex flex-row items-center justify-between">
       <div className="flex flex-col">
@@ -33,26 +61,7 @@ const Counter: React.FC<CounterProps> = ({
         <div className="font-light text-gray-600">{subtitle}</div>
       </div>
       <div className="flex flex-row items-center gap-4">
-        {/* create reusable to button below */}
-        <div
-          onClick={onReduce}
-          className="
-            w-10
-            h-10
-            rounded-full
-            border-[1px]
-            border-neutral-400
-            flex
-            items-center
-            justify-center
-            text-neutral-600
-            cursor-pointer
-            hover:opacity-80
-            transition
-          "
-        >
-          <AiOutlineMinus />
-        </div>
+        <VaryButton onClick={onReduce} isMinor />
         <div
           className="
             font-light 
@@ -62,25 +71,7 @@ const Counter: React.FC<CounterProps> = ({
         >
           {value}
         </div>
-        <div
-          onClick={onAdd}
-          className="
-            w-10
-            h-10
-            rounded-full
-            border-[1px]
-            border-neutral-400
-            flex
-            items-center
-            justify-center
-            text-neutral-600
-            cursor-pointer
-            hover:opacity-80
-            transition
-          "
-        >
-          <AiOutlinePlus />
-        </div>
+        <VaryButton onClick={onAdd} />
       </div>
     </div>
   );
