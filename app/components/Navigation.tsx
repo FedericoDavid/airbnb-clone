5b407b84531166303113a5c0a75a06ff57a7c9c5
Federico_Enriquez <fedenri98@gmail.com>
2024-11-01 01:08:36 -0300

    feat: navbar border bottom hover

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -4,17 +4,23 @@ interface NavigationProps {}
 
 const Navigation: React.FC<NavigationProps> = () => {
   return (
-    <div className={`w-full md:w-auto py-2 font-semibold text-white`}>
+    <div className="w-full md:w-auto py-2 font-semibold text-white">
       <div className="flex flex-row items-center justify-between">
-        <div className="px-6 text-sm hidden sm:block hover:text-gold">
-          <p className="hover:text-gold cursor-pointer">Find a Home</p>
+        <div className="px-6 text-sm hidden sm:block">
+          <p className="border-b-[1px] border-transparent hover:border-gold cursor-pointer">
+            Find a Home
+          </p>
         </div>
         <div className="hidden sm:block text-sm px-6 border-x-[1px] border-gold flex-1 text-center">
-          <p className="hover:text-gold cursor-pointer">My Home value</p>
+          <p className="border-b-[1px] border-transparent hover:border-gold cursor-pointer">
+            My Home value
+          </p>
         </div>
         <div className="text-sm pl-6 pr-2 flex flex-row items-center gap-3">
-          <div className="hidden sm:block p-2">
-            <p className="hover:text-gold cursor-pointer">Rent a Home</p>
+          <div className="hidden sm:block">
+            <p className="border-b-[1px] border-transparent hover:border-gold cursor-pointer">
+              Rent a Home
+            </p>
           </div>
         </div>
       </div>
